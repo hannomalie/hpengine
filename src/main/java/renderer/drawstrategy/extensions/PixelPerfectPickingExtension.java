1a9d99fb94ff71e7fc8069237da723adc0a3043a
Hannes Pernpeintner <hannes.pernpeintner@gmx.de>
2016-08-29 08:19:20 +0200

    adjusted picking for resizable window

--- src/main/java/renderer/drawstrategy/extensions/PixelPerfectPickingExtension.java
+++ src/main/java/renderer/drawstrategy/extensions/PixelPerfectPickingExtension.java
@@ -1,11 +1,14 @@
 package renderer.drawstrategy.extensions;
 
+import config.Config;
 import engine.AppContext;
 import engine.model.Entity;
+import event.ClickEvent;
 import event.EntitySelectedEvent;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector2f;
 import renderer.OpenGLContext;
 import renderer.RenderExtract;
 import renderer.drawstrategy.FirstPassResult;
@@ -28,7 +31,11 @@ public class PixelPerfectPickingExtension implements RenderExtension {
             OpenGLContext.getInstance().readBuffer(4);
 
             floatBuffer.rewind();
-            GL11.glReadPixels(Mouse.getX(), Mouse.getY(), 1, 1, GL11.GL_RGBA, GL11.GL_FLOAT, floatBuffer);
+            Vector2f ratio = new Vector2f(Float.valueOf(Config.WIDTH) / Float.valueOf(AppContext.getInstance().getFrame().getWidth()),
+                    Float.valueOf(Config.HEIGHT) / Float.valueOf(AppContext.getInstance().getFrame().getHeight()));
+            int adjustedX = (int) (Mouse.getX() * ratio.x);
+            int adjustedY = (int) (Mouse.getY() * ratio.y);
+            GL11.glReadPixels(adjustedX, adjustedY, 1, 1, GL11.GL_RGBA, GL11.GL_FLOAT, floatBuffer);
             try {
                 int componentIndex = 3; // alpha component
                 appContext.getScene().getEntities().parallelStream().forEach(e -> {
