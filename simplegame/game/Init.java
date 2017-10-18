b6c9bb00bd5d96673c6d703cfff89c22b94cd497
Hannes Pernpeintner <hannes.pernpeintner@gmx.de>
2017-10-18 23:11:04 +0200

    changed init scripts

--- simplegame/game/Init.java
+++ simplegame/game/Init.java
@@ -1,16 +1,17 @@
-import de.hanno.hpengine.engine.model.loader.md5.AnimationController;
-import de.hanno.hpengine.engine.transform.SimpleSpatial;
-import de.hanno.hpengine.engine.transform.Transform;
 import de.hanno.hpengine.engine.DirectoryManager;
 import de.hanno.hpengine.engine.Engine;
-import de.hanno.hpengine.engine.component.JavaComponent;
+import de.hanno.hpengine.engine.component.ModelComponent;
 import de.hanno.hpengine.engine.graphics.renderer.command.LoadModelCommand;
 import de.hanno.hpengine.engine.lifecycle.LifeCycle;
 import de.hanno.hpengine.engine.model.Cluster;
 import de.hanno.hpengine.engine.model.Entity;
 import de.hanno.hpengine.engine.model.Instance;
+import de.hanno.hpengine.engine.model.Model;
+import de.hanno.hpengine.engine.model.loader.md5.AnimationController;
+import de.hanno.hpengine.engine.model.material.Material;
 import de.hanno.hpengine.engine.model.material.MaterialFactory;
-import de.hanno.hpengine.util.ressources.CodeSource;
+import de.hanno.hpengine.engine.transform.SimpleSpatial;
+import de.hanno.hpengine.engine.transform.Transform;
 import org.joml.Vector3f;
 
 import java.io.File;
@@ -20,8 +21,8 @@ public class Init implements LifeCycle {
 
     private boolean initialized;
 
-    int maxDistance = 15;
-    int clusterDistance = 10*maxDistance;
+    int maxDistance = 175;
+    int clusterDistance = 4*maxDistance;
     Vector3f[] clusterLocations = {new Vector3f(clusterDistance, 0, clusterDistance),
             new Vector3f(clusterDistance, 0, -clusterDistance),
             new Vector3f(-clusterDistance, 0, -clusterDistance),
@@ -31,7 +32,7 @@ public class Init implements LifeCycle {
     public void init() {
 
         try {
-            LoadModelCommand.EntityListResult loaded = new LoadModelCommand(new File(DirectoryManager.WORKDIR_NAME + "/assets/models/cube.obj"), "cube").execute(Engine.getInstance());
+            LoadModelCommand.EntityListResult loaded = new LoadModelCommand(new File(DirectoryManager.WORKDIR_NAME + "/assets/models/doom3monster/monster.md5mesh"), "cube").execute(Engine.getInstance());
             System.out.println("loaded entities : " + loaded.entities.size());
             for(final Entity current : loaded.entities) {
 //                File componentScriptFile = new File(Engine.getInstance().getDirectoryManager().getGameDir() + "/scripts/SimpleMoveComponent.java");
@@ -40,14 +41,15 @@ public class Init implements LifeCycle {
                 for(int clusterIndex = 0; clusterIndex < 5; clusterIndex++) {
                     Cluster cluster = new Cluster();
                     Random random = new Random();
-                    int count = 5;
+                    int count = 2;
                     for(int x = -count; x < count; x++) {
                         for(int y = -count; y < count; y++) {
                             for(int z = -count; z < count; z++) {
                                 Transform trafo = new Transform();
                                 float randomFloat = random.nextFloat() - 0.5f;
                                 trafo.setTranslation(new Vector3f().add(new Vector3f(clusterLocations[clusterIndex%clusterLocations.length])).add(new Vector3f(randomFloat* maxDistance *x,randomFloat* maxDistance *y,randomFloat* maxDistance *z)));
-                                cluster.add(new Instance(trafo, MaterialFactory.getInstance().getMaterialsAsList().get((x+y+z+3*count)%10), new AnimationController(0,0), new SimpleSpatial(){
+                                Material material = current.getComponent(ModelComponent.class, ModelComponent.COMPONENT_KEY).getMaterial();//MaterialFactory.getInstance().getMaterialsAsList().get((x + y + z + 3 * count) % 10);
+                                cluster.add(new Instance(trafo, material, new AnimationController(120,24 + randomFloat*7), new SimpleSpatial(){
                                     @Override
                                     public Vector3f[] getMinMax() {
                                         return current.getMinMax();
