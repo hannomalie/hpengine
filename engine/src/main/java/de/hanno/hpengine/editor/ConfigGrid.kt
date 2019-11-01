ff59481a19e278ac18966825fd0fc7dc3bc9c8d8
Hannes Tenter <tenter@e-spirit.com>
2019-11-01 19:31:28 +0100

    added legend panels and other config options

--- engine/src/main/java/de/hanno/hpengine/editor/ConfigGrid.kt
+++ engine/src/main/java/de/hanno/hpengine/editor/ConfigGrid.kt
@@ -1,6 +1,5 @@
 package de.hanno.hpengine.editor
 
-import de.hanno.hpengine.engine.config.DebugConfig
 import de.hanno.hpengine.engine.config.SimpleConfig
 import net.miginfocom.swing.MigLayout
 import javax.swing.JCheckBox
@@ -9,21 +8,37 @@ import javax.swing.JLabel
 import javax.swing.JPanel
 import kotlin.reflect.KMutableProperty1
 import kotlin.reflect.KProperty1
-import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.declaredMembers
 import kotlin.reflect.jvm.javaType
+import javax.swing.BorderFactory
+
 
 class ConfigGrid(val config: SimpleConfig): JPanel() {
     init {
-        layout = MigLayout("wrap 4")
-        DebugConfig::class.declaredMemberProperties.forEach {
-            if(it.returnType.javaType == Boolean::class.java ) {
-                val checkBox = when {
-                    it is KMutableProperty1<*, *> -> (it as? KMutableProperty1<DebugConfig, Boolean>)?.toCheckBox(config.debug)
-                    else -> (it as? KProperty1<DebugConfig, Boolean>)?.toCheckBox(config.debug)
+        layout = MigLayout("wrap 1")
+        getInputsPanelForObject(config.debug, "Debug")?.let { add(it) }
+        getInputsPanelForObject(config.effects, "Effects")?.let { add(it) }
+        getInputsPanelForObject(config.performance, "Performance")?.let { add(it) }
+        getInputsPanelForObject(config.quality, "Quality")?.let { add(it) }
+    }
+
+    private inline fun <reified T> getInputsPanelForObject(debugConfig: T, legend: String): JPanel? {
+
+        val checkBoxes = T::class.declaredMembers.mapNotNull {
+            if (it.returnType.javaType == Boolean::class.java) {
+                when (it) {
+                    is KMutableProperty1<*, *> -> (it as? KMutableProperty1<T, Boolean>)?.toCheckBox(debugConfig)
+                    else -> (it as? KProperty1<T, Boolean>)?.toCheckBox(debugConfig)
                 }
-                if(checkBox != null) add(checkBox)
-            }
+            } else null
         }
+        return if(checkBoxes.isNotEmpty()) {
+            JPanel().apply {
+                layout = MigLayout("wrap 4")
+                border = BorderFactory.createTitledBorder(legend)
+                checkBoxes.forEach { this.add(it) }
+            }
+        } else null
     }
 
     private fun <R> KMutableProperty1<R, Boolean>.toCheckBox(receiver: R): JCheckBox {
