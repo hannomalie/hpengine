f6583ce7ee7047e5a386f82af9653e7434db8ca3
Hannes Tenter <tenter@e-spirit.com>
2019-11-16 14:29:25 +0100

    converted vertexindexbuffer to kotlin

---
+++ engine/src/main/java/de/hanno/hpengine/engine/scene/VertexIndexBuffer.kt
@@ -0,0 +1,35 @@
+package de.hanno.hpengine.engine.scene
+
+import de.hanno.hpengine.engine.graphics.GpuContext
+import de.hanno.hpengine.engine.model.DataChannels
+import de.hanno.hpengine.engine.model.IndexBuffer
+import de.hanno.hpengine.engine.model.VertexBuffer
+import org.lwjgl.BufferUtils
+
+import java.io.Serializable
+import java.util.EnumSet
+
+class VertexIndexBuffer(gpuContext: GpuContext<*>,
+                        vertexBufferSizeInFloatsCount: Int,
+                        indexBufferSizeInIntsCount: Int,
+                        channels: EnumSet<DataChannels>) {
+
+    var vertexBuffer = VertexBuffer(gpuContext, BufferUtils.createFloatBuffer(vertexBufferSizeInFloatsCount), channels)
+    var indexBuffer = IndexBuffer(gpuContext, BufferUtils.createIntBuffer(indexBufferSizeInIntsCount))
+    private var currentBaseVertex = 0
+    private var currentIndexOffset = 0
+
+    fun allocate(elementsCount: Int, indicesCount: Int): VertexIndexOffsets = synchronized(this) {
+        VertexIndexOffsets(currentBaseVertex, currentIndexOffset).apply {
+            currentBaseVertex += elementsCount
+            currentIndexOffset += indicesCount
+        }
+    }
+
+    fun resetAllocations() {
+        currentBaseVertex = 0
+        currentIndexOffset = 0
+    }
+
+    data class VertexIndexOffsets(val vertexOffset: Int, val indexOffset: Int)
+}
