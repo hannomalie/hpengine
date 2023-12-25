7646937059092637c12ea236f5aa01b8b2148216
Hannes Tenter <hannes.tenter@gmail.com>
2023-12-25 20:15:28 +0100

    make directional light move with priary cam

--- engine/src/main/kotlin/de/hanno/hpengine/graphics/light/directional/DirectionalLight.kt
+++ engine/src/main/kotlin/de/hanno/hpengine/graphics/light/directional/DirectionalLight.kt
@@ -5,22 +5,27 @@ import com.artemis.BaseSystem
 import com.artemis.ComponentMapper
 import com.artemis.World
 import com.artemis.annotations.All
+import com.artemis.managers.TagManager
 import de.hanno.hpengine.Transform
 import de.hanno.hpengine.WorldPopulator
 import de.hanno.hpengine.artemis.forEachEntity
 import de.hanno.hpengine.artemis.forFirstEntityIfPresent
+import de.hanno.hpengine.artemis.getOrNull
 import de.hanno.hpengine.component.CameraComponent
 import de.hanno.hpengine.component.NameComponent
 import de.hanno.hpengine.component.TransformComponent
+import de.hanno.hpengine.component.primaryCameraTag
 import de.hanno.hpengine.cycle.CycleSystem
 import de.hanno.hpengine.graphics.state.RenderState
 import de.hanno.hpengine.input.Input
+import de.hanno.hpengine.spatial.WorldAABB
 import de.hanno.hpengine.system.Extractor
 import de.hanno.hpengine.transform.EntityMovementSystem
 import org.joml.AxisAngle4f
 import org.joml.Matrix4f
 import org.joml.Quaternionf
 import org.joml.Vector3f
+import org.joml.Vector3i
 import org.koin.core.annotation.Single
 import org.lwjgl.glfw.GLFW
 import struktgen.api.forIndex
@@ -32,6 +37,8 @@ class DirectionalLightSystem(
     private val directionalLightStateHolder: DirectionalLightStateHolder,
     private val input: Input,
     private val entityMovementSystem: EntityMovementSystem,
+    private val worldAABB: WorldAABB,
+    private val tagManager: TagManager,
 ) : BaseEntitySystem(), Extractor, WorldPopulator {
     lateinit var cycleSystem: CycleSystem
     lateinit var directionalLightComponentMapper: ComponentMapper<DirectionalLightComponent>
@@ -45,8 +52,23 @@ class DirectionalLightSystem(
         val degreesPerSecond = 45f
         val rotateAmount = Math.toRadians(degreesPerSecond.toDouble()).toFloat() * world.delta
 
+        val primaryCamera = tagManager.getEntityId(primaryCameraTag)
+        val primaryCameraTrafo = if(primaryCamera == -1) null else transformComponentMapper.getOrNull(primaryCamera)
+
         forEachEntity {
+            val camera = cameraComponentMapper[it]
+            camera.width = 2 * worldAABB.aabb.boundingSphereRadius
+            camera.height = 2 * worldAABB.aabb.boundingSphereRadius
+
             val transform = transformComponentMapper[it].transform
+
+            primaryCameraTrafo?.let {
+                val orientation = transform.transformation.getRotation(AxisAngle4f())
+                val c = it.transform.center
+                val ci = Vector3i(c.x.toInt(), c.y.toInt(), c.z.toInt())
+                transform.translation(Vector3f(ci.x.toFloat(), ci.y.toFloat(), ci.z.toFloat()))
+                transform.rotate(orientation)
+            }
 
             if (input.isKeyPressed(GLFW.GLFW_KEY_UP)) {
                 transform.rotateAround(Vector3f(0f, 1f, 0f), rotateAmount, Vector3f())
@@ -59,18 +81,6 @@ class DirectionalLightSystem(
             }
             if (input.isKeyPressed(GLFW.GLFW_KEY_RIGHT)) {
                 transform.rotateAround(Vector3f(1f, 0f, 0f), -rotateAmount, Vector3f())
-            }
-            if (input.isKeyPressed(GLFW.GLFW_KEY_8)) {
-                transform.translate(Vector3f(0f, -moveAmount, 0f))
-            }
-            if (input.isKeyPressed(GLFW.GLFW_KEY_2)) {
-                transform.translate(Vector3f(0f, moveAmount, 0f))
-            }
-            if (input.isKeyPressed(GLFW.GLFW_KEY_4)) {
-                transform.translate(Vector3f(-moveAmount, 0f, 0f))
-            }
-            if (input.isKeyPressed(GLFW.GLFW_KEY_6)) {
-                transform.translate(Vector3f(moveAmount, 0f, 0f))
             }
         }
     }
--- engine/src/main/kotlin/de/hanno/hpengine/spatial/WorldAABB.kt
+++ engine/src/main/kotlin/de/hanno/hpengine/spatial/WorldAABB.kt
@@ -2,26 +2,38 @@ package de.hanno.hpengine.spatial
 
 import com.artemis.BaseEntitySystem
 import com.artemis.BaseSystem
+import com.artemis.ComponentMapper
 import com.artemis.annotations.All
+import de.hanno.hpengine.artemis.forEachEntity
 import de.hanno.hpengine.component.TransformComponent
 import de.hanno.hpengine.graphics.state.RenderState
 import de.hanno.hpengine.graphics.state.RenderStateContext
+import de.hanno.hpengine.model.ModelCacheComponent
 import de.hanno.hpengine.scene.MinMax
 import de.hanno.hpengine.system.Extractor
 import de.hanno.hpengine.transform.AABB
+import de.hanno.hpengine.transform.AABBData
 import org.joml.Vector3f
 import org.koin.core.annotation.Single
 
-@Single(binds = [BaseSystem::class, WorldAABB::class])
-@All(TransformComponent::class, SpatialComponent::class)
+@Single(binds = [BaseSystem::class, WorldAABB::class, Extractor::class])
+@All(TransformComponent::class, ModelCacheComponent::class)
 class WorldAABB(
     private val worldAABBStateHolder: WorldAABBStateHolder,
 ): BaseEntitySystem(), Extractor {
-    val aabb = AABB(Vector3f(100f)) // TODO: Find sensible default?
+    lateinit var modelCacheComponentMapper: ComponentMapper<ModelCacheComponent>
+    lateinit var transformComponentComponentMapper: ComponentMapper<TransformComponent>
+
+    val aabb = AABBData(Vector3f(-100f), Vector3f(100f)) // TODO: Find sensible default?
 
     override fun inserted(entityId: Int) {
-//        TODO: Implement
-//        aabb.localAABB = calculateAABB()
+        forEachEntity {
+            val other = modelCacheComponentMapper[it].model.boundingVolume.apply {
+                recalculate(transformComponentComponentMapper[it].transform.transformation)
+            }
+            aabb.updateWith(other)
+        }
+        aabb.update()
     }
 
     override fun extract(currentWriteState: RenderState) {
@@ -29,6 +41,16 @@ class WorldAABB(
         currentWriteState[worldAABBStateHolder.worldAABBState].max.set(aabb.max)
     }
     override fun processSystem() { }
+}
+
+private fun AABBData.updateWith(other: AABB) {
+    min.x = minOf(min.x, other.min.x)
+    min.y= minOf(min.y, other.min.y)
+    min.z = minOf(min.z, other.min.z)
+
+    max.x = maxOf(max.x, other.max.x)
+    max.y= maxOf(max.y, other.max.y)
+    max.z = maxOf(max.z, other.max.z)
 }
 
 @Single
